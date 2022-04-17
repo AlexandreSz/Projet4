e6c687242ce8cc2c6d99c44246bfc695b09f536f
AlexandreSz <75409769+AlexandreSz@users.noreply.github.com>
2022-04-17 14:58:30 +0200

    Ajout du test validation Mail

--- valid_form.js
+++ valid_form.js
@@ -1,6 +1,8 @@
 const prenom = document.getElementById('first');
 const nom = document.getElementById('last');
+const mail = document.getElementById('email');
 const regex = /^[a-zA-Z][a-z]+([-'][a-zA-z][a-z]+)?/;
+const regexMail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 const input = document.getElementsByClassName('text-control');
 const form = document.getElementById('form');
 
@@ -46,6 +48,25 @@ function checkNom(e) {
     }
 }
 
+function checkMail(e) {
+    if (email.value.trim() === '') {
+        e.preventDefault();
+        mail.parentElement.setAttribute('data-error-visible', 'true');
+        mail.parentElement.setAttribute('data-error', "Veuillez remplir le champ");
+        mail.style.border = '2px solid #e54858';
+        return false;
+    } else if (!mail.value.match(regexMail)) {
+        e.preventDefault();
+        mail.parentElement.setAttribute('data-error-visible', 'true');
+        mail.parentElement.setAttribute('data-error', "Veuillez entrer un email valide (ex : toto@gmail.com)");
+        mail.style.border = '2px solid #e54858';
+        return false;
+    } else {
+        email.parentElement.setAttribute('data-error-visible', 'false');
+        email.style.border = 'solid #279e7a 0.19rem';
+        return true;
+    }
+}
 
 // FORM FIELDS EVENTS
 function formFieldsValidation(element, method, event) {
@@ -53,18 +74,21 @@ function formFieldsValidation(element, method, event) {
 }
 formFieldsValidation(prenom, checkPrenom, 'focusout');
 formFieldsValidation(nom, checkNom, 'focusout');
+formFieldsValidation(mail, checkMail, 'focusout');
 
 
 // FOR ALL FIELDS VALIDATION
 function forAllFieldsValidation() {
     checkPrenom()
     checkNom()
+    checkMail()
 
 }
 
 function formValidation() {
     if (checkPrenom() === true &&
-        checkNom() === true) {
+        checkNom() === true &&
+        checkMail() === true) {
         return true;
     }
     return false;
